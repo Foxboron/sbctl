2b09a8e645377407de566cf6ce8faefda01ee846
Érico Rolim <erico.erc@gmail.com>
2020-07-13 22:19:38 -0300

    Change PrintGenerateError signature.
    This avoid boilerplate calls to fmt.Sprintf in the main code.

--- util.go
+++ util.go
@@ -4,11 +4,13 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
 )
 
-func PrintGenerateError(msg string, logger *log.Logger) error {
+func PrintGenerateError(logger *log.Logger, msg string, args ...interface{}) error {
+	msg = fmt.Sprintf(msg, args)
 	logger.Println(msg)
 	return errors.New(msg)
 }
