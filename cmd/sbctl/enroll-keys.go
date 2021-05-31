13bbb90c035a0ea9a1aaf8d714e08002bf168b23
Morten Linderud <morten@linderud.pw>
2021-05-31 22:46:09 +0200

    sbctl/enroll-keys: Refactor immutable checking
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- cmd/sbctl/enroll-keys.go
+++ cmd/sbctl/enroll-keys.go
@@ -9,24 +9,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func CheckImmutable() error {
+	var isImmutable bool
+	for _, file := range sbctl.EfivarFSFiles {
+		err := sbctl.IsImmutable(file)
+		if errors.Is(err, sbctl.ErrImmutable) {
+			isImmutable = true
+			logging.Warn("File is immutable: %s", file)
+		} else if errors.Is(err, sbctl.ErrNotImmutable) {
+			continue
+		} else if err != nil {
+			return fmt.Errorf("couldn't read file: %s", file)
+		}
+	}
+	if isImmutable {
+		return sbctl.ErrImmutable
+	}
+	return nil
+}
+
 var enrollKeysCmd = &cobra.Command{
 	Use:   "enroll-keys",
 	Short: "Enroll the current keys to EFI",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		var isImmutable bool
-		for _, file := range sbctl.EfivarFSFiles {
-			err := sbctl.IsImmutable(file)
-			if errors.Is(err, sbctl.ErrImmutable) {
-				isImmutable = true
-				logging.Warn("File is immutable: %s", file)
-			} else if errors.Is(err, sbctl.ErrNotImmutable) {
-				continue
-			} else if err != nil {
-				return fmt.Errorf("couldn't read file: %s", file)
-			}
-		}
-		if isImmutable {
-			return sbctl.ErrImmutable
+		if err := CheckImmutable(); err != nil {
+			return err
 		}
 		logging.Print("Syncing keys to EFI variables...")
 		synced := sbctl.SBKeySync(sbctl.KeysPath)
