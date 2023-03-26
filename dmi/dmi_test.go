85c9bfd9c32064f8db7807d7dfd29eaf673fb695
Dawid Potocki <dawid@dawidpotocki.com>
2023-03-26 14:12:20 +1300

    dmi: Test all used DMI fields

--- dmi/dmi_test.go
+++ dmi/dmi_test.go
@@ -45,6 +45,15 @@ func TestDMIParse(t *testing.T) {
 	if dmiTable.FirmwareDate != time.Date(2023, 1, 13, 0, 0, 0, 0, time.UTC) {
 		t.Fatal("FirmwareDate: expected '2023-01-13', got '" + dmiTable.FirmwareDate.Format("2006-01-02") + "'")
 	}
+	if dmiTable.FirmwareRelease != "HorribleFirmwareRelease" {
+		t.Fatal("FirmwareRelease: expected 'HorribleFirmwareRelease', got '" + dmiTable.FirmwareRelease + "'")
+	}
+	if dmiTable.FirmwareVendor != "EmbarrassedFirmwareVendor" {
+		t.Fatal("FirmwareVendor: expected 'EmbarrassedFirmwareVendor', got '" + dmiTable.FirmwareVendor + "'")
+	}
+	if dmiTable.FirmwareVersion != "InsecureFirmwareVersion" {
+		t.Fatal("FirmwareVersion: expected 'InsecureFirmwareVersion', got '" + dmiTable.FirmwareVersion + "'")
+	}
 	if dmiTable.ProductFamily != "MediocreProductFamily" {
 		t.Fatal("ProductFamily: expected 'MediocreProductFamily', got '" + dmiTable.ProductFamily + "'")
 	}
@@ -52,7 +61,7 @@ func TestDMIParse(t *testing.T) {
 		t.Fatal("ProductName: expected 'AwfulProductName', got '" + dmiTable.ProductName + "'")
 	}
 	if dmiTable.ProductSKU != "RandomProductSKU" {
-		t.Fatal("RandomProductSKU: expected 'RandomProductSKU', got '" + dmiTable.ProductSKU + "'")
+		t.Fatal("ProductSKU: expected 'RandomProductSKU', got '" + dmiTable.ProductSKU + "'")
 	}
 	if dmiTable.ProductVersion != "CrazyProductVersion" {
 		t.Fatal("ProductVersion: expected 'CrazyProductVersion' , got '" + dmiTable.ProductVersion + "'")
