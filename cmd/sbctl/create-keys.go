65044f78100e410e7cc3196f1dac624713650c72
Luke Kuzmish <lakuzmish@gmail.com>
2023-11-10 20:11:46 -0500

    allow specifying keys and GUID paths

--- cmd/sbctl/create-keys.go
+++ cmd/sbctl/create-keys.go
@@ -8,21 +8,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	exportPath   string = sbctl.KeysPath
+	databasePath string = sbctl.DatabasePath
+)
+
 var createKeysCmd = &cobra.Command{
 	Use:   "create-keys",
 	Short: "Create a set of secure boot signing keys",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if err := sbctl.CreateDirectory(sbctl.KeysPath); err != nil {
+		if err := sbctl.CreateDirectory(exportPath); err != nil {
 			return err
 		}
-		uuid, err := sbctl.CreateGUID(sbctl.DatabasePath)
+		uuid, err := sbctl.CreateGUID(databasePath)
 		if err != nil {
 			return err
 		}
 		logging.Print("Created Owner UUID %s\n", uuid)
-		if !sbctl.CheckIfKeysInitialized(sbctl.KeysPath) {
+		if !sbctl.CheckIfKeysInitialized(exportPath) {
 			logging.Print("Creating secure boot keys...")
-			err := sbctl.InitializeSecureBootKeys(sbctl.KeysPath)
+			err := sbctl.InitializeSecureBootKeys(exportPath)
 			if err != nil {
 				logging.NotOk("")
 				return fmt.Errorf("couldn't initialize secure boot: %w", err)
@@ -36,7 +41,15 @@ var createKeysCmd = &cobra.Command{
 	},
 }
 
+func createKeysCmdFlags(cmd *cobra.Command) {
+	f := cmd.Flags()
+	f.StringVarP(&exportPath, "export", "e", "", "export file path. defaults to "+sbctl.KeysPath)
+	f.StringVarP(&databasePath, "database-path", "d", "", "location to create GUID file. defaults to "+sbctl.DatabasePath)
+}
+
 func init() {
+	createKeysCmdFlags(createKeysCmd)
+
 	CliCommands = append(CliCommands, cliCommand{
 		Cmd: createKeysCmd,
 	})
